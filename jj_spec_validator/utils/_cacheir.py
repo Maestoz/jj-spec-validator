e8d6944e25db956067940df836896db9c9ef1997
Maestoz <S.A.Roz@yandex.ru>
2024-07-26 00:36:19 +0700

    refactor matcher

--- jj_spec_validator/utils/_cacheir.py
+++ jj_spec_validator/utils/_cacheir.py
@@ -18,11 +18,11 @@ CACHE_DIR = 'cache_parsed_specs'
 CACHE_TTL = 3600  # in second
 
 
-def _build_entity_dict(entities: List[SchemaData]) -> Dict[Tuple[str, str], SchemaData]:
+def _build_entity_dict(entities: List[SchemaData], prefix: str | None = None) -> Dict[Tuple[str, str], SchemaData]:
     entity_dict = {}
     for entity in entities:
-        normalized_path = normalize_path(entity.path)
-        entity_key = (entity.http_method.lower(), normalized_path)
+        normalized_path = normalize_path(entity.path, prefix=prefix)
+        entity_key = (entity.http_method.upper(), normalized_path)
         entity_dict[entity_key] = entity
     return entity_dict
 
@@ -61,7 +61,7 @@ def _save_cache(spec_link: str, raw_schema) -> None:
         dump(raw_schema, f)
 
 
-def load_cache(spec_link: str) -> Dict[Tuple[str, str], SchemaData]:
+def load_cache(spec_link: str, prefix: str | None = None) -> Dict[Tuple[str, str], SchemaData]:
     filename = _get_cache_filename(spec_link)
 
     if _validate_cache_file(filename):
@@ -74,6 +74,6 @@ def load_cache(spec_link: str) -> Dict[Tuple[str, str], SchemaData]:
         _save_cache(spec_link, raw_schema)
 
     parsed_data = collect_schema_data(raw_schema)
-    prepared_dict = _build_entity_dict(parsed_data)
+    prepared_dict = _build_entity_dict(parsed_data, prefix=prefix)
 
     return prepared_dict
--- jj_spec_validator/validate_spec.py
+++ jj_spec_validator/validate_spec.py
@@ -7,9 +7,17 @@ from d42 import validate_or_fail
 from schemax_openapi import SchemaData
 from jj import RelayResponse
 from jj.matchers import ResolvableMatcher
+from jj.matchers import MethodMatcher as JJMethodMatcher
+from jj.matchers import PathMatcher as JJPathMatcher
+from jj.matchers.attribute_matchers import RouteMatcher as JJRouteMatcher
+from jj.matchers import AnyMatcher as JJAnyMatcher
+from jj.matchers import AllMatcher as JJAllMatcher
+from jj.matchers import EqualMatcher as JJEqualMatcher
 from .utils import load_cache, normalize_path
 from revolt.errors import SubstitutionError
 from aiohttp.web_urldispatcher import DynamicResource
+from dataclasses import dataclass
+
 
 from valera import ValidationException
 
@@ -17,16 +25,17 @@ _T = TypeVar('_T')
 
 
 class BaseMatcher:
-    async def match(self, method: str, path: str) -> bool:
+    def match(self, spec_unit: tuple[str, str]) -> bool:
         raise NotImplementedError()
 
 
-class EqualMatcher:
-    def __init__(self, expected: Any) -> None:
-        self._expected = expected
+class MethodMatcher(BaseMatcher):
+    def __init__(self, mocked_method: Any) -> None:
+        self._mocked_method = mocked_method
 
-    async def match(self, actual: Any) -> bool:
-        return bool(self._expected == actual)
+    def match(self, spec_unit: tuple[str, str]) -> bool:
+        print(f"{self._mocked_method} == {spec_unit[0]}", bool(self._mocked_method == spec_unit[0]))
+        return bool(self._mocked_method == spec_unit[0])
 
 
 class _Resource(DynamicResource):
@@ -34,55 +43,39 @@ class _Resource(DynamicResource):
         return self._match(path)
 
 
-class MethodMatcher(BaseMatcher):
-    def __init__(self, method: Any) -> None:
-        self._matcher = EqualMatcher(str.upper(method))
+class RouteMatcher(BaseMatcher):
+    def __init__(self, mocked_path: str) -> None:
+        self._mocked_path = mocked_path
+        self._resource = _Resource(mocked_path)
 
-    async def match(self, method: str, path: str) -> bool:
-        return await self._matcher.match("*") or await self._matcher.match(method)
-
-
-class RouteMatcher:
-    def __init__(self, path: str) -> None:
-        self._path = path
-        self._resource = _Resource(path)
-
-    async def match(self, path: str) -> bool:
-        return self._resource.match(path) is not None
-
-    def get_segments(self, path: str) -> Dict[str, str]:
-        return self._resource.match(path) or {}
-
-
-class PathMatcher(BaseMatcher):
-    def __init__(self, path: str) -> None:
-        self._matcher = RouteMatcher(path)
-
-    async def match(self, method: str, path: str) -> bool:
-        matched = await self._matcher.match(path)
-        return matched
+    def match(self, spec_unit: tuple[str, str]) -> bool:
+        self._resource_spec = _Resource(spec_unit[1])
+        mock = normalize_path(self._mocked_path)
+        spec = normalize_path(spec_unit[1])
+        print(f"{mock} == {spec}", (mock == spec) or (self._resource_spec.match(self._mocked_path) is not None))
+        return (mock == spec) or (self._resource_spec.match(self._mocked_path) is not None)
 
 
 class AnyMatcher(BaseMatcher):
-    def __init__(self, matchers: List[BaseMatcher]) -> None:
+    def __init__(self, matchers: list[BaseMatcher]) -> None:
         assert len(matchers) > 0
         self._matchers = matchers
 
-    async def match(self, method: str, path: str) -> bool:
+    def match(self, spec_unit: tuple[str, str]) -> bool:
         for matcher in self._matchers:
-            if await matcher.match(method, path):
+            if matcher.match(spec_unit):
                 return True
         return False
 
 
 class AllMatcher(BaseMatcher):
-    def __init__(self, matchers: List[BaseMatcher]) -> None:
+    def __init__(self, matchers: list[BaseMatcher]) -> None:
         assert len(matchers) > 0
         self._matchers = matchers
 
-    async def match(self, method: str, path: str) -> bool:
+    def match(self, spec_unit: tuple[str, str]) -> bool:
         for matcher in self._matchers:
-            if not await matcher.match(method, path):
+            if not matcher.match(spec_unit):
                 return False
         return True
 
@@ -95,6 +88,28 @@ class Validator:
         normalized_path = normalize_path(path)
         entity_key = (http_method.lower(), normalized_path)
         return entity_dict.get(entity_key)
+
+    @staticmethod
+    def create_openapi_matcher(matcher: ResolvableMatcher, prefix: str | None = None) -> BaseMatcher:
+        if isinstance(matcher, JJMethodMatcher):
+            submatcher = matcher.sub_matcher
+            if isinstance(submatcher, JJEqualMatcher):
+                spec_matcher = MethodMatcher(mocked_method=submatcher.expected)
+                return spec_matcher
+        if isinstance(matcher, JJPathMatcher):
+            submatcher = matcher.sub_matcher
+            if isinstance(submatcher, JJRouteMatcher):
+                mocked_path = normalize_path(submatcher.path, prefix=prefix)
+                spec_matcher = RouteMatcher(mocked_path=mocked_path)
+                return spec_matcher
+        if isinstance(matcher, JJAllMatcher):
+            submatchers = matcher.sub_matchers
+            spec_matcher = AllMatcher(matchers=[Validator.create_openapi_matcher(submatcher, prefix=prefix) for submatcher in submatchers])
+            return spec_matcher
+        if isinstance(matcher, JJAnyMatcher):
+            submatchers = matcher.sub_matchers
+            spec_matcher = AnyMatcher(matchers=[Validator.create_openapi_matcher(submatcher, prefix=prefix) for submatcher in submatchers])
+            return spec_matcher
 
     @staticmethod
     def _handle_non_strict_validation_error(parsed_request: Optional[SchemaData],
@@ -112,16 +127,6 @@ class Validator:
             elif validate_level == "skip":
                 pass
 
-    @staticmethod
-    def create_openapi_matcher(matcher: ResolvableMatcher) -> BaseMatcher:
-        pass
-
-    @staticmethod
-    def get_matched_schemas(openapi_spec: Dict[str, Any], matcher: BaseMatcher) -> List[SchemaData]:
-        ...
-        # for (method, path), schema in openapi_spec:
-        #     if match(mathod, path):
-        #         return schema
 
     @staticmethod
     def validate(mocked: _T,
@@ -131,21 +136,36 @@ class Validator:
                  func_name: str,
                  prefix: str | None) -> None:
         mock_matcher = mocked.handler.matcher
-        spec_matcher = Validator.create_openapi_matcher(matcher=mock_matcher)
 
-        # spec_matcher.match(spec.method, spec.path)  # bool
+        # debug_method = MethodMatcher(mocked_method='get')
+        # debug_match_method = debug_method.match(spec_unit=('get', '3.0/'))
+        #
+        # debug_path = RouteMatcher(mocked_path='/2.0/{id}/{another_id}/bob/alice')
+        # debug_match_path = debug_path.match(spec_unit=('get', '/2.0/{id_segment}/{id_submodule}/bob/alice'))  # now True, and True exp
+        #
+        # debug_path_2 = RouteMatcher(mocked_path='/2.0/2352352352/bob/alice')
+        # debug_match_path_2 = debug_path_2.match(spec_unit=('get', '/2.0/{id_segment}/bob/alice'))  # now False, but True exp
+        #
+        # debug_path_3 = RouteMatcher(mocked_path='/2.0/413412341/2352352352/bob/alice')
+        # debug_match_path_3 = debug_path_3.match(spec_unit=('get', '/2.0/{id_segment}/{id_submodule}/bob/alice'))  # now False, but True exp
+        #
+        # debug_all_matcher = AllMatcher(matchers=[debug_method, debug_path])
+        # debug_match_all_matcher = debug_all_matcher.match(spec_unit=('post', '/2.0/{id_segment}/{id_submodule}/bob/alice'))
+        #
+        # debug_any_matcher = AnyMatcher(matchers=[debug_method, debug_path])
+        # debug_match_any_matcher = debug_any_matcher.match(spec_unit=('post', '/2.0/{id_segment}/{id_submodule}/bob/alice/a'))
+        print("\n  \n")
+        spec_matcher = Validator.create_openapi_matcher(matcher=mock_matcher, prefix=prefix)
+        print("\n  \n")
 
-        # schemas = get_matched_schemas(openapi_spec, spec_matcher)  # чаще всего, тут одна схема
-        # response.body == schema.any(*scheams)
+        matched_spec_units = [(http_method, path) for http_method, path in prepared_dict_from_spec.keys() if spec_matcher.match((http_method, path))]
 
-        matcher = mocked.handler.matcher.sub_matchers  # type: ignore
-        method = matcher[0].sub_matcher.expected
-        path = normalize_path(matcher[1].sub_matcher.path, prefix)
+        assert len(matched_spec_units) == 1, "matched more than one spec unit or nothing matched"
 
-        parsed_request = Validator._check_entity_match(prepared_dict_from_spec, http_method=method, path=path)
+        spec_unit = prepared_dict_from_spec.get(matched_spec_units[0])
 
-        if parsed_request:
-            if parsed_request.response_schema_d42:
+        if spec_unit:
+            if spec_unit.response_schema_d42:
                 try:
                     # check for JSON in response of mock
                     decoded_mocked_body = loads(mocked.handler.response.get_body().decode())  # type: ignore
@@ -154,26 +174,26 @@ class Validator:
 
                 if is_strict:
                     try:
-                        validate_or_fail(parsed_request.response_schema_d42, decoded_mocked_body)
+                        validate_or_fail(spec_unit.response_schema_d42, decoded_mocked_body)
                     except ValidationException as e:
                         raise ValidationException(f"There are some mismatches in {func_name}:{str(e)}")
 
-                    Validator._handle_non_strict_validation_error(parsed_request, decoded_mocked_body, validate_level,
+                    Validator._handle_non_strict_validation_error(spec_unit, decoded_mocked_body, validate_level,
                                                                   func_name)
                 else:
-                    Validator._handle_non_strict_validation_error(parsed_request, decoded_mocked_body, validate_level,
+                    Validator._handle_non_strict_validation_error(spec_unit, decoded_mocked_body, validate_level,
                                                                   func_name)
 
             else:
-                raise AssertionError(f"API method '{method} {path}' in the spec_link"
+                raise AssertionError(f"API method '{prepared_dict_from_spec}' in the spec_link"
                                      f" lacks a response structure for the validation of {func_name}")
         else:
-            raise AssertionError(f"API method '{method} {path}' was not found in the spec_link "
+            raise AssertionError(f"API method '{prepared_dict_from_spec}' was not found in the spec_link "
                                  f"for the validation of {func_name}")
 
 
-def _prepare_data(spec_link: str) -> Dict[Tuple[str, str], SchemaData]:
-    return load_cache(spec_link)
+def _prepare_data(spec_link: str, prefix: str | None = None) -> Dict[Tuple[str, str], SchemaData]:
+    return load_cache(spec_link, prefix=prefix)
 
 
 def validate_spec(*,
@@ -193,6 +213,10 @@ def validate_spec(*,
     def decorator(func: Callable[..., _T]) -> Callable[..., _T]:
         func_name = func.__name__
 
+        # non_sctrict_path_matcher = PathMatcher(path='/__reviews__/2.0/reviews/{review_id}/comments')
+        # sctrict_path_matcher = PathMatcher(path='/__reviews__/2.0/reviews/123123/comments')
+        # test_request_path = '/__reviews__/2.0/reviews/123123/comments'
+
         @wraps(func)
         async def async_wrapper(*args: object, **kwargs: object) -> _T:
             if spec_link:
@@ -200,7 +224,7 @@ def validate_spec(*,
                 if isinstance(mocked.handler.response, RelayResponse):
                     print("RelayResponse type is not supported")
                     return mocked
-                prepared_dict_from_spec = _prepare_data(spec_link)
+                prepared_dict_from_spec = _prepare_data(spec_link, prefix=prefix)
                 Validator.validate(mocked, prepared_dict_from_spec, is_strict, validate_level, func_name, prefix)
             else:
                 mocked = await func(*args, **kwargs)
@@ -213,7 +237,8 @@ def validate_spec(*,
                 if isinstance(mocked.handler.response, RelayResponse):
                     print("RelayResponse type is not supported")
                     return mocked
-                prepared_dict_from_spec = _prepare_data(spec_link)
+                prepared_dict_from_spec = _prepare_data(spec_link, prefix=prefix)
+
                 Validator.validate(mocked, prepared_dict_from_spec, is_strict, validate_level, func_name, prefix)
             else:
                 mocked = func(*args, **kwargs)
