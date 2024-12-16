e8170c87eb34baadccfe4abc356aa34f102756e4
Maestoz <S.A.Roz@yandex.ru>
2024-12-16 20:49:05 +0700

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def find_required():
 
 setup(
     name="jj-spec-validator",
-    version="0.1.19",
+    version="0.1.20",
     description="jj mocks validator for openapi specs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
